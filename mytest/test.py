6d93c6c35542d20da4c4f49c6543e8b05cce3b73
jph <jiaopenghui123@gmail.com>
2022-07-18 18:48:21 +0800

    push test

--- mytest/test.py
+++ mytest/test.py
@@ -5,3 +5,4 @@ browser_driver.get('https://www.jd.com/')
 add_cookies(browser_driver, 'jd.json')  # 添加用户信息cookies,模拟登录
 print('this is master modify demo by')
 print('this is a modify conflict demo by hot-fix')
+print('push test')
