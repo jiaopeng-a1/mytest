ad5c46658e781a034ee7fadddd268a7ba99eea0d
jph <jiaopenghui123@gmail.com>
2022-07-18 16:42:06 +0800

    京东爬虫v1.0.1

--- mytest/middlewares.py
+++ mytest/middlewares.py
@@ -78,7 +78,7 @@ class MytestDownloaderMiddleware:
 
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders......
+        # This method is used by Scrapy to create your spiders....
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
